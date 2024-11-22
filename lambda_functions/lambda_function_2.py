196f4540f948e5072c3a70c6e82a489a7f17484f
lucianodinizsts <102624692+lucianodinizsts@users.noreply.github.com>
2024-11-22 19:32:58 -0300

    fixing json

--- lambda_functions/lambda_function_2.py
+++ lambda_functions/lambda_function_2.py
@@ -1,5 +1,7 @@
+import json
+
 def lambda_handler(event, context):
     return {
-        'statusCode': 200,
-        'body': 'Olá, sou a função 2'
+        "statusCode": 200,
+        "body": json.dumps("Olá, sou a função 2")
     }
